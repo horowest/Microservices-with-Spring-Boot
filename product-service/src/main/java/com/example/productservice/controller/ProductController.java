64a19dd0f6543abb5e902e331e3acd2f5585c8a6
Akash Saha <akash5@protonmail.com>
2023-04-22 15:04:27 +0000

    first microservice

--- product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.example.productservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.productservice.dto.ProductRequest;
+import com.example.productservice.dto.ProductResponse;
 import com.example.productservice.service.ProductService;
 
 @RestController
@@ -22,5 +26,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
         productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getProducts() {
+        return productService.getProducts();
     }
 }
--- product-service/src/main/java/com/example/productservice/service/ProductService.java
+++ product-service/src/main/java/com/example/productservice/service/ProductService.java
@@ -1,9 +1,14 @@
 package com.example.productservice.service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.example.productservice.dto.ProductResponse;
 import com.example.productservice.dto.ProductRequest;
 import com.example.productservice.model.Product;
 import com.example.productservice.repository.ProductRepository;
@@ -21,14 +26,30 @@ public class ProductService {
     public void createProduct(ProductRequest productRequest) {
         // build product from ProductRequest
         Product product = Product.builder()
-        .name(productRequest.getName())
-        .desciption(productRequest.getDescription())
-        .price(productRequest.getPrice())
-        .build();
+                .name(productRequest.getName())
+                .desciption(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
 
         // save productr to db
         productRepository.save(product);
         log.info("Product {} is saved.", product.getName());
+    }
+
+    public List<ProductResponse> getProducts() {
+        return productRepository.findAll().stream()
+                .map(e -> mapProductToProductResponse(e))
+                .collect(Collectors.toList());
+    }
+
+    private ProductResponse mapProductToProductResponse(Product product) {
+        ProductResponse productResponse = ProductResponse.builder()
+                .name(product.getName())
+                .description(product.getDesciption())
+                .price(product.getPrice())
+                .build();
+
+        return productResponse;
     }
     
 }
